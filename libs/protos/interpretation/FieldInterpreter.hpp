f2082271b6a4ce9485f6feb22097dcc64ca4b0f9
Sebastian Messerschmidt <sebastian_messerschmidt@baxter.com>
2025-03-31 21:49:49 +0200

    add interpreter implementation

---
+++ libs/protos/interpretation/FieldInterpreter.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <protos/interpretation/FieldInterpretation.hpp>
+#include <protos/interpretation/InterpretationResult.hpp>
+#include <protos/interpretation/GenericTypes.hpp>
+#include <protos/interpretation/TypeFormatter.hpp>
+
+#include <vector>
+#include <string>
+#include <optional>
+#include <cstddef>
+#include <ranges>
+#include <algorithm>
+
+namespace protos::interpreter {
+
+struct FieldInterpretation;
+
+class FieldInterpreter
+{
+public:
+    static std::vector<std::string> toStringResult(const protos::interpreter::InterpretationResults& result)
+    {
+        std::vector<std::string> res;
+        std::ranges::transform(result, std::back_inserter(res), [](const auto& r) {
+            return datafw::value::variant_to_formatted_string(r.value, "{}");
+        });
+        return res;
+    }
+
+    static protos::interpreter::InterpretationResults interpret(
+        const std::string&                             name,
+        const std::vector<std::byte>&                  data,
+        const protos::interpreter::FieldInterpretation&  interpretation,
+        const protos::interpreter::InterpretationResults& context);
+    /**
+     * @brief: Checks if the field can be interpreted safely.This encompasses checking if the field is of the correct
+     * size for the type and if the format string is valid
+     * @return std::optional<std::string> - empty if the field is valid, otherwise a string describing the error
+     */
+    // std::optional<std::string> validate() const;
+};
+
+} // namespace datafw::dissector
---
+++ libs/protos/interpretation/GenericPacketInterpreter.cpp
@@ -0,0 +1,87 @@
+#include "GenericPacketInterpreter.hpp"
+#include "TypeFormatter.hpp"
+#include "FieldInterpretation.hpp"
+#include "FieldInterpreter.hpp"
+#include <common/Once.hpp>
+
+// #include <datafw/utils/once.hpp>
+// #include <fmt/format.h>
+// #include <magic_enum.hpp>
+// #include <spdlog/spdlog.h>
+
+bool protos::interpreter::GenericPacketInterpreter::hasField(const std::string& name) const
+{
+    return fields.find(name) != fields.end();
+}
+
+const protos::interpreter::FieldInterpretation& protos::interpreter::GenericPacketInterpreter::findInterpreterForField(
+    const std::string& name) const
+{
+    auto it = fields.find(name);
+    if (it != fields.end()) { return (*it).second; }
+    else
+    {
+        throw std::runtime_error(
+            std::format("Interpreter for field {} not found in GenericPacketInterpreter {}", name, getName()));
+    };
+}
+
+void protos::interpreter::GenericPacketInterpreter::addField(FieldInterpretation&& fieldInterpreter)
+{
+    fields.emplace(fieldInterpreter.name, std::move(fieldInterpreter));
+}
+
+protos::interpreter::InterpretationResults protos::interpreter::GenericPacketInterpreter::interpretPacketData(
+    const protos::PacketData& data) const
+{
+    protos::interpreter::InterpretationResults result;
+    for (const auto& field : data.fields)
+    {
+        const auto has_field = hasField(field.name);
+        if (!has_field) { handleMissingField(field.name, result); }
+
+        else
+        {
+            auto& interpreter = findInterpreterForField(field.name);
+            auto  res = protos::interpreter::FieldInterpreter::interpret(field.name, field.value, interpreter, result);
+            result.insert(result.end(), res.begin(), res.end());
+        }
+    }
+
+    return result;
+}
+
+// void protos::interpreter::GenericPacketInterpreter::handlePacketData(const protos::PacketData& data) const
+// {
+//     if (interpretationResultsCallback) { interpretationResultsCallback(interpretPacketData(data)); }
+//     else
+//     {
+//         throw std::runtime_error(
+//             std::format("No callback set for interpretation results in GenericPacketInterpreter {}", getName()));
+//     }
+// }
+
+void protos::interpreter::GenericPacketInterpreter::handleMissingField(
+    const std::string&                       name,
+    protos::interpreter::InterpretationResults& intpretation_result) const
+{
+    switch (behaviors.emptyFieldBehavior)
+    {
+    default:
+        [[fallthrough]];
+    case EmptyFieldBehavior::SKIP:
+        break;
+    case EmptyFieldBehavior::SKIP_AND_WARN_ONCE:
+        protos::utils::once(name + getName(), [&]() {
+            throw std::runtime_error("decide if we use logger here");
+            // spdlog::warn("Field '{}' not found in GenericPacketInterpreter '{}'", name, getName());
+        });
+        break;
+    case EmptyFieldBehavior::EMPTY_STRING:
+        intpretation_result.push_back({name, datafw::value::Variant{""}});
+        break;
+    case EmptyFieldBehavior::DASH:
+        intpretation_result.push_back({name, datafw::value::Variant{"-"}});
+        break;
+    }
+}
