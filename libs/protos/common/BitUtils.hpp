d65b5696f9d10fc19f2057a2edb95aae2560fb45
Sebastian Messerschmidt <sebastian_messerschmidt@baxter.com>
2025-05-13 11:39:08 +0200

    GCC build fixes

--- libs/protos/common/BitUtils.hpp
+++ libs/protos/common/BitUtils.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <span>
 #include <ranges>
+#include <cstring>
 
 namespace protos::bytes {
 
--- libs/protos/dissection/FieldDescriptor.hpp
+++ libs/protos/dissection/FieldDescriptor.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <cstddef>
+#include <cstdint>
 
 namespace protos::dissector {
 
