88acd9e49008e5394e769989aede900202d8f74e
Sebastian Messerschmidt <sebastian_messerschmidt@baxter.com>
2026-03-25 12:13:21 +0100

    Coverage only for GNU/Clang and if Flag is set

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,4 +1,10 @@
+option(ENABLE_COVERAGE "Enable code coverage instrumentation (GCC/Clang on Unix-like only)" OFF)
+
 function(enable_coverage project_name)
+  if(NOT ENABLE_COVERAGE)
+    return()
+  endif()
+
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
     target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
     target_link_libraries(${project_name} INTERFACE --coverage)
